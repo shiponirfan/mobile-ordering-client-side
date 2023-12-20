7d3cbf782e26f409d36ccf71af665384b0e13860
shiponirfan <imshiponirfan@gmail.com>
2023-12-20 20:12:53 +0600

    Mobile Ordering Website

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Shared/Navbar/Navbar";
+
+const MainLayout = () => {
+    return (
+        <div>
+           <Navbar/>
+        </div>
+    );
+};
+
+export default MainLayout;
